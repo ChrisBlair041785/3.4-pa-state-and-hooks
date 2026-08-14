6fe6881bd4f03592526afb95113085a9d6a8975f
Chris <chris.blair041785@gmail.com>
2026-08-14 10:56:08 -0400

    Initial upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Button from './Button';
+import TextField from './TextField';
+import TodoList from './TodoList';
 
 function App() {
+  
+  const [count, setCount] = useState(0); 
+  const handleIncrement = () => {
+    setCount(count + 1); 
+  }; 
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }; 
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Counter: {count}</h1>
+        <Button label="Increment" onClick={handleIncrement}>Increment</Button>
+        <Button label="Decrement" onClick={handleDecrement}>Decrement</Button>
+        <Button label="Reset" onClick={handleReset}>Reset</Button>
+        <h1>Text Field Value: {inputValue}</h1>
+        <TextField value={inputValue} onChange={handleInputChange} />
+        <h1>Todo List:</h1>
+        <TodoList />
       </header>
     </div>
   );
